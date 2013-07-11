a46475f5b5e1197e65c22ebc71a0cbc00519726d
Elmer Mendoza <defreddyelmer@gmail.com>
2013-07-11 00:43:06 -0400

    Create model and controller for product

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Presale::Application.routes.draw do
+  resources :products
+
+  devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  devise_for :users
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130711042733) do
+ActiveRecord::Schema.define(version: 20130711043539) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.decimal  "price",      precision: 10, scale: 2, default: 0.0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
